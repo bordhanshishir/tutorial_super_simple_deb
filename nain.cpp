c4026e5e819b4c23d0550b3db209022fce7c94c4
Shishir Bordhan <shishirbordhan@gmail.com>
2024-09-16 00:28:10 +0600

    version 0.4

--- nain.cpp
+++ nain.cpp
@@ -1,10 +1,10 @@
-// version 0.3
+// version 0.4
 
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    cout << "hello world1" << endl;
+    cout << "hello world1!" << endl;
     return 0;
 }
